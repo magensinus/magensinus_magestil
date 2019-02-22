ecdcec867f2b656a41502a782832c6d4e4071d79
Tony de Faria <tonydefaria@iigethr.com>
2019-02-22 12:38:49 +0000

    added recent articles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,6 +11,11 @@ module ApplicationHelper
     @settings = Settings.first
   end
 
+  # Recent journal articles
+  def recent_journal_articles
+    @recent_journal_articles ||= Journal::Article.where(published: true, magestil: true).order(published_at: :desc).limit(10)
+  end
+
   # Vcard
   def vcard
     @vcard ||= Contact.first!
