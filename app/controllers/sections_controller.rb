f97c4e289a9b492bbbc8a769b9dbab60f1e8c520
Tony de Faria <tony.faria@icloud.com>
2022-09-14 14:55:21 +0200

    add tutors

--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -5,6 +5,8 @@ class SectionsController < ApplicationController
   def show
     @course ||= Academy::Course.find_by(slug: params[:course_id])
     @course_section ||= @course.sections.find_by(slug: params[:id])
+
+    @tutors = @course.course_tutors.where(academy_course_id: @course.id).order(position: :asc)
     respond_to do |format|
       format.js
     end
